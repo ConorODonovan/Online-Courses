4a01ec515db5fd8eb2dbdda500b88f900a2a05ae
Conor O'Donovan <conor.m.odonovan@gmail.com>
2020-03-29 13:58:18 +0100

    More joins

--- Udemy/SQL/Joins/CustomerOrders.sql
+++ Udemy/SQL/Joins/CustomerOrders.sql
@@ -44,3 +44,13 @@ JOIN orders
 	ON customers.id = orders.customer_id
 GROUP BY orders.customer_id
 ORDER BY total_spent DESC;
+
+SELECT
+	first_name,
+    last_name,
+    IFNULL(SUM(amount), 0) AS total_spent
+FROM customers
+LEFT JOIN orders
+	ON customers.id = orders.customer_id
+GROUP BY customers.id
+ORDER BY total_spent;
