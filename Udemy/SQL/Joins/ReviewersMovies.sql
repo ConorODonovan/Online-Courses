61f41b6e8099fd90a4aabd898f11243b2951ec05
Conor O'Donovan <conor.m.odonovan@gmail.com>
2020-03-29 17:33:13 +0100

    More joins

--- Udemy/SQL/Joins/ReviewersMovies.sql
+++ Udemy/SQL/Joins/ReviewersMovies.sql
@@ -77,3 +77,11 @@ JOIN reviews
     ON series.id = reviews.series_id
 GROUP BY series.id
 ORDER BY avg_rating;
+
+SELECT
+    first_name,
+    last_name,
+    rating
+FROM reviews
+INNER JOIN reviewers
+    ON reviewers.id = reviews.reviewer_id;
